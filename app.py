95fdd87444b96557af5c60a08af30d2f62c2abfa
Nicki Anselmo <nickianselmo@gmail.com>
2015-06-03 19:21:11 +0000

    added dictionary hash and translate functionality

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from translate import translate
 import os
 
 app = Flask(__name__)
@@ -9,14 +10,15 @@ app = Flask(__name__)
 @app.route('/')
 def show_index():
     #return "Hello"
-    return render_template('index.html')
+    return render_template('index.html', submitted = False)
 
 
 @app.route('/', methods=['POST'])
 def display_translation():
     original_text = request.form['text_to_translate']
-    return original_text
-
+    new_text = translate(original_text)
+    return render_template('index.html', submitted = True, translated_text = new_text)
+    
 # @app.route('/hello/')
 # @app.route('/hello/<name>')
 # def hello(name=None):
