27bdca1abfe1940fad2deeb4c1b387fcd4d9ec23
Nicki Anselmo <nickianselmo@gmail.com>
2015-06-03 18:08:23 +0000

    put index.html in templates folder, added sumbit button with a sample POST action

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-
+import os
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def show_index():
+    #return "Hello"
     return render_template('index.html')
 
 
 @app.route('/', methods=['POST'])
-#def translate():
-	original_text = request.form['textbox']
-	return render_template('results.html', text_to_translate=original_text)
+def display_translation():
+    original_text = request.form['text_to_translate']
+    return original_text
 
 # @app.route('/hello/')
 # @app.route('/hello/<name>')
@@ -22,4 +23,4 @@ def show_index():
 #     return render_template('hello.html', name=name)
 
 if __name__ == '__main__':
-    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
+    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)), debug=True)
